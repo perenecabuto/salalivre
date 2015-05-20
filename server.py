b7b6b9dfc2a341cdef38ae1f6d01482d69f7c6be
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2015-05-20 04:09:08 -0300

    Event rotation

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 from flask import Flask, render_template, request, jsonify
 from flask.ext.pymongo import PyMongo
-from bson.json_util import dumps
 
 app = Flask(__name__)
 mongo = PyMongo(app)
@@ -27,7 +26,7 @@ def rooms():
         if room:
             return "Room %s already exists" % _id, 409
 
-        mongo.db.rooms.insert({'_id': _id})
+        mongo.db.rooms.insert({'_id': _id, 'events': []})
         return "", 201
 
     rooms = mongo.db.rooms.find() or []
@@ -38,15 +37,22 @@ def rooms():
 def room(_id):
     if request.method == 'GET':
         room = mongo.db.rooms.find_one_or_404({'_id': _id})
-        return dumps(room)
+        return jsonify(room)
     elif request.method == 'DELETE':
-        return mongo.db.rooms.remove({'_id': _id})
+        mongo.db.rooms.remove({'_id': _id})
+        return "", 200
 
 
 @app.route("/room/<_id>/event", methods=['POST'])
 def register_event(_id):
-    room = mongo.db.rooms.find_and_modify({'_id': _id}, {'$push': {'events': datetime.now().isoformat()}})
-    return jsonify(room=room), 404 if room is None else 201
+    room = mongo.db.rooms.find_one({'_id': _id})
+    if room is None:
+        return "", 404
+
+    events = room.get('events', [])
+    events.append(datetime.now())
+    room = mongo.db.rooms.find_and_modify({'_id': _id}, {'events': events[-10:]})
+    return "",  201
 
 
 if __name__ == "__main__":
